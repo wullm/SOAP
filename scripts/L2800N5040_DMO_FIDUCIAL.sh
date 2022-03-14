af0930ee48d9f912bdceb2614b68f7c7959a3c1d
John Helly <j.c.helly@durham.ac.uk>
2022-03-14 10:53:48 +0000

    Added scripts for FLAMINGO runs

---
+++ scripts/L2800N5040_DMO_FIDUCIAL.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -l
+#
+#SBATCH --nodes=4
+#SBATCH --cpus-per-task=1
+#SBATCH -J test_halo_props
+#SBATCH -o ./test_halo_props.out
+#SBATCH -p cosma8
+#SBATCH -A dp004
+#SBATCH --exclusive
+#SBATCH -t 4:00:00
+#SBATCH --reservation=jlvc76_53
+#
+
+module purge
+module load gnu_comp/11.1.0 openmpi/4.1.1 python/3.10.1
+
+export HDF5_USE_FILE_LOCKING=FALSE
+
+swift_filename="/cosma8/data/dp004/flamingo/Runs/L2800N5040/DMO_FIDUCIAL/snapshots/flamingo_0078/flamingo_0078.%(file_nr)d.hdf5"
+vr_basename=/cosma8/data/dp004/flamingo/Runs/L2800N5040/DMO_FIDUCIAL/VR/catalogue_0078/vr_catalogue_0078.properties
+chunks_per_dimension=3
+outfile=/cosma8/data/dp004/jch/FLAMINGO/tmp/test.hdf5
+
+mpirun python3 -u -m mpi4py ./compute_halo_properties.py \
+    ${swift_filename} ${vr_basename} ${chunks_per_dimension} ${outfile}
---
+++ scripts/test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+#
+#SBATCH --nodes=4
+#SBATCH --cpus-per-task=1
+#SBATCH -J test
+#SBATCH -o ./test.out
+#SBATCH -p cosma8
+#SBATCH -A dp004
+#SBATCH --exclusive
+#SBATCH -t 0:10:00
+#
+
+module purge
+module load gnu_comp/11.1.0 openmpi/4.1.1 python/3.10.1
+
+swift_filename=/cosma8/data/dp004/jch/FLAMINGO/BlackHoles/200_w_lightcone/snapshots/flamingo_0013.hdf5
+vr_basename=/cosma8/data/dp004/jch/FLAMINGO/BlackHoles/200_w_lightcone/vr/catalogue_0013/vr_catalogue_0013.properties
+chunks_per_dimension=1
+outfile=/cosma8/data/dp004/jch/FLAMINGO/BlackHoles/200_w_lightcone/test_output/test.hdf5
+
+mpirun python3 -m mpi4py \
+    ./compute_halo_properties.py ${swift_filename} ${vr_basename} ${chunks_per_dimension} ${outfile}
