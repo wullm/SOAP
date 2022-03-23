991f4869cd6fd0cc438933f1cc9c5b05296d0183
John Helly <j.c.helly@durham.ac.uk>
2022-03-23 14:20:02 +0000

    Updates to test scripts

--- scripts/test_group_membership.sh
+++ scripts/test_group_membership.sh
@@ -15,7 +15,11 @@ module load gnu_comp/11.1.0 openmpi/4.1.1 python/3.10.1
 
 swift_filename="/cosma8/data/dp004/jch/FLAMINGO/BlackHoles/200_w_lightcone/snapshots/flamingo_0013.hdf5"
 vr_basename="/cosma8/data/dp004/jch/FLAMINGO/BlackHoles/200_w_lightcone/vr/catalogue_0013/vr_catalogue_0013"
-outfile="/cosma8/data/dp004/jch/tmp/test.hdf5"
+outfile="/cosma8/data/dp004/jch/FLAMINGO/BlackHoles/200_w_lightcone/group_membership/vr_membership_0013.hdf5"
+
+outdir=`dirname "${outfile}"`
+\mkdir -p ${outdir}
+lfs setstripe --stripe-count=-1 --stripe-size=32M ${outdir}
 
 mpirun python3 -u -m mpi4py \
     ./vr_group_membership.py ${swift_filename} ${vr_basename} ${outfile}
--- scripts/test_halo_properties.sh
+++ scripts/test_halo_properties.sh
@@ -2,11 +2,12 @@
 #
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
+#SBATCH --tasks-per-node=16
 #SBATCH -J test
 #SBATCH -o ./test.out
-#SBATCH -p cosma8
+#SBATCH -p cosma8-shm
 #SBATCH -A dp004
-#SBATCH --exclusive
+##SBATCH --exclusive
 #SBATCH -t 0:20:00
 #
 
@@ -14,9 +15,10 @@ module purge
 module load gnu_comp/11.1.0 openmpi/4.1.1 python/3.10.1
 
 swift_filename=/cosma8/data/dp004/jch/FLAMINGO/BlackHoles/200_w_lightcone/snapshots/flamingo_0013.hdf5
-vr_basename=/cosma8/data/dp004/jch/FLAMINGO/BlackHoles/200_w_lightcone/vr/catalogue_0013/vr_catalogue_0013.properties
+vr_basename=/cosma8/data/dp004/jch/FLAMINGO/BlackHoles/200_w_lightcone/vr/catalogue_0013/vr_catalogue_0013
 chunks_per_dimension=1
 outfile=/cosma8/data/dp004/jch/FLAMINGO/BlackHoles/200_w_lightcone/test_output/test.hdf5
+extra_filename=/cosma8/data/dp004/jch/FLAMINGO/BlackHoles/200_w_lightcone/group_membership/vr_membership_0013.hdf5
 
 mpirun python3 -m mpi4py \
-    ./compute_halo_properties.py ${swift_filename} ${vr_basename} ${chunks_per_dimension} ${outfile}
+    ./compute_halo_properties.py ${swift_filename} ${vr_basename} ${chunks_per_dimension} ${outfile} ${extra_filename}
