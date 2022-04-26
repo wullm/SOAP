ab365dcc589431e5a20b23bb499f6edd07a17c6c
John Helly <j.c.helly@durham.ac.uk>
2022-04-26 14:30:54 +0100

    Update FLAMINGO job scripts

---
+++ scripts/FLAMINGO/L1000N1800/halo_properties_L1000N1800.sh
@@ -0,0 +1,49 @@
+#!/bin/bash -l
+#
+# Compute halo properties for a snapshot. Must run the group_membership
+# script first.
+#
+# Job name determines which of the L1000N1800 runs we process.
+# Array job index is the snapshot number to do. Submit with (for example):
+#
+# sbatch -J HYDRO_FIDUCIAL --array=78 ./halo_properties_L1000N1800.sh
+#
+#SBATCH --nodes=4
+#SBATCH --cpus-per-task=1
+#SBATCH -J test_halo_props
+#SBATCH -o ./logs/halo_properties_L1000N1800_%x.%a.out
+#SBATCH -p cosma8
+#SBATCH -A dp004
+#SBATCH --exclusive
+#SBATCH -t 4:00:00
+#
+
+module purge
+module load gnu_comp/11.1.0 openmpi/4.1.1 python/3.10.1
+
+# Which snapshot to do
+snapnum=`printf '%04d' ${SLURM_ARRAY_TASK_ID}`
+
+# Which simulation to do
+sim="L1000N1800/${SLURM_JOB_NAME}"
+
+# Input simulation location
+basedir="/cosma8/data/dp004/flamingo/Runs/L1000N1800/${sim}/"
+
+# Where to write the output
+outbase="/cosma8/data/dp004/jch/FLAMINGO/ScienceRuns/${sim}/"
+
+# Generate file names
+swift_filename="${basedir}/snapshots/flamingo_${snapnum}/flamingo_${snapnum}.%(file_nr)d.hdf5"
+extra_filename="${outbase}/group_membership/vr_membership_${snapnum}.%(file_nr)d.hdf5"
+vr_basename="${basedir}/VR/catalogue_${snapnum}/vr_catalogue_${snapnum}"
+outfile="${outbase}/${sim}/halo_properties/halo_properties_${snapnum}.hdf5"
+
+chunks_per_dimension=2
+
+# Create output directory
+outdir=`dirname "${outfile}"`
+mkdir -p "${outdir}"
+
+mpirun python3 -u -m mpi4py ./compute_halo_properties.py \
+    ${swift_filename} ${vr_basename} ${chunks_per_dimension} ${outfile} ${extra_filename}
--- scripts/test_group_membership.sh
+++ scripts/test_group_membership.sh
@@ -2,20 +2,22 @@
 #
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
-#SBATCH -J test
-#SBATCH -o ./test.out
+#SBATCH -J test_group_membership
+#SBATCH -o ./test_group_membership.out
 #SBATCH -p cosma8
 #SBATCH -A dp004
 #SBATCH --exclusive
-#SBATCH -t 0:20:00
+#SBATCH -t 1:00:00
 #
 
 module purge
 module load gnu_comp/11.1.0 openmpi/4.1.1 python/3.10.1
 
-swift_filename="/cosma8/data/dp004/jch/FLAMINGO/BlackHoles/200_w_lightcone/snapshots/flamingo_0013.hdf5"
-vr_basename="/cosma8/data/dp004/jch/FLAMINGO/BlackHoles/200_w_lightcone/vr/catalogue_0013/vr_catalogue_0013"
-outfile="/cosma8/data/dp004/jch/FLAMINGO/BlackHoles/200_w_lightcone/group_membership/vr_membership_0013.hdf5"
+snapnum=`printf '%04d' ${SLURM_ARRAY_TASK_ID}`
+
+swift_filename="/cosma8/data/dp004/jch/FLAMINGO/BlackHoles/200_w_lightcone/snapshots/flamingo_${snapnum}.hdf5"
+vr_basename="/cosma8/data/dp004/jch/FLAMINGO/BlackHoles/200_w_lightcone/vr/catalogue_${snapnum}/vr_catalogue_${snapnum}"
+outfile="/cosma8/data/dp004/jch/FLAMINGO/BlackHoles/200_w_lightcone/group_membership/vr_membership_${snapnum}.hdf5"
 
 mpirun python3 -u -m mpi4py \
     ./vr_group_membership.py ${swift_filename} ${vr_basename} ${outfile}
