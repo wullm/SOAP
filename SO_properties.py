Removed box wrapping from subhalo calculation. Fixed some errors after merge. Made sure central particle is at r=0 for unit tests. Added check for particle properties that are read by each object and added missing one to SOProperties.

SO_properties.py
@@ -177,7 +177,7 @@ class SOProperties(HaloProperty):
             "XrayLuminosities",
             "XrayPhotonLuminosities",
         ],
-        "PartType1": ["Coordinates", "Masses", "Velocities"],
+        "PartType1": ["Coordinates", "GroupNr_bound", "Masses", "Velocities"],
         "PartType4": [
             "Coordinates",
             "GroupNr_bound",
@@ -981,7 +981,7 @@ def test_SO_properties():
 
     for i in range(100):
         input_halo, data, rmax, Mtot, Npart = dummy_halos.get_random_halo(
-            [1, 10, 100, 1000, 10000], has_neutrinos=True
+            [2, 10, 100, 1000, 10000], has_neutrinos=True
         )
         rho_ref = Mtot / (4.0 / 3.0 * np.pi * rmax**3)
 
@@ -1049,12 +1049,18 @@ def test_SO_properties():
                     0.1 * rmax,
                     "Dummy value to force correct behaviour",
                 )
+            input_data = {}
+            for ptype in prop_calc.particle_properties:
+                if ptype in data:
+                    input_data[ptype] = {}
+                    for dset in prop_calc.particle_properties[ptype]:
+                        input_data[ptype][dset] = data[ptype][dset]
             input_halo_copy = input_halo.copy()
-            data_copy = data.copy()
-            prop_calc.calculate(input_halo, rmax, data, halo_result)
+            input_data_copy = input_data.copy()
+            prop_calc.calculate(input_halo, rmax, input_data, halo_result)
             # make sure the calculation does not change the input
             assert input_halo_copy == input_halo
-            assert data_copy == data
+            assert input_data_copy == input_data
 
             for name, size, dtype, unit_string, _ in prop_calc.SO_properties:
                 full_name = f"SO/{SO_name}/{name}"

subhalo_properties.py
@@ -216,8 +216,6 @@ class SubhaloProperties(HaloProperty):
             in_halo = grnr == index
             mass.append(data[ptype][mass_dataset(ptype)][in_halo])
             pos = data[ptype]["Coordinates"][in_halo, :] - centre[None, :]
-            pos[pos < -0.5 * self.boxsize] += self.boxsize
-            pos[pos >= 0.5 * self.boxsize] -= self.boxsize
             position.append(pos)
             r = np.sqrt(pos[:, 0] ** 2 + pos[:, 1] ** 2 + pos[:, 2] ** 2)
             radius.append(r)
@@ -441,7 +439,7 @@ def test_subhalo_properties():
                         input_data[ptype][dset] = data[ptype][dset]
             input_halo_copy = input_halo.copy()
             input_data_copy = input_data.copy()
-            prop_calc.calculate(input_halo, input_data, halo_result)
+            prop_calc.calculate(input_halo, 0.*unyt.kpc, input_data, halo_result)
             assert input_halo == input_halo_copy
             assert input_data == input_data_copy
 

test_SO_radius_calculation.py
@@ -8,7 +8,7 @@ matplotlib.use("Agg")
 import matplotlib.pyplot as pl
 
 from SO_properties import find_SO_radius_and_mass
-from halo_properties import SearchRadiusTooSmallError
+from halo_properties import ReadRadiusTooSmallError
 
 
 def test_SO_radius_calculation():
@@ -39,11 +39,11 @@ def test_SO_radius_calculation():
         reference_density = 200.0 * Mpart * npart / (4.0 * np.pi / 3.0 * rmax**3)
 
         try:
-            SO_r, SO_mass = find_SO_radius_and_mass(
+            SO_r, SO_mass, SO_volume = find_SO_radius_and_mass(
                 ordered_radius, density, cumulative_mass, reference_density
             )
             print(f"{i:03d}: SO_r: {SO_r}, SO_mass: {SO_mass}")
-        except SearchRadiusTooSmallError:
+        except ReadRadiusTooSmallError:
             print(f"{i:03d}: Radius too small!")
             SO_r = -1.0 * unyt.kpc
             SO_mass = -1.0 * unyt.Msun