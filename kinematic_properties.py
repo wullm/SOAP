Added TotalAxisLengths to subhalo and SO properties. Fixed numerical issues in axis length calculation.

kinematic_properties.py
@@ -141,7 +141,9 @@ def get_axis_lengths(mass, position):
 
     # linalg.eigenvals cannot deal with units anyway, so we have to add them
     # back in
-    axes = np.sqrt(np.linalg.eigvals(Itensor)) * position.units
+    axes = np.linalg.eigvals(Itensor).real
+    axes = np.clip(axes, 0., None)
+    axes = np.sqrt(axes) * position.units
 
     # sort the axes from long to short
     axes.sort()