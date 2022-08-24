7947a7702db98c46d6af71a7249642a624bebe24
John Helly <j.c.helly@durham.ac.uk>
2022-08-24 14:50:38 +0100

    Update test scripts for 200Mpc run

--- scripts/test_group_membership.sh
+++ scripts/test_group_membership.sh
@@ -4,8 +4,8 @@
 #SBATCH --cpus-per-task=1
 #SBATCH -J test_group_membership
 #SBATCH -o ./logs/test_group_membership.%a.out
-#SBATCH -p cosma
-#SBATCH -A durham
+#SBATCH -p cosma7-shm
+#SBATCH -A dp004
 #SBATCH --exclusive
 #SBATCH -t 2:00:00
 #
@@ -14,11 +14,11 @@ module purge
 module load gnu_comp/11.1.0 openmpi/4.1.1 python/3.10.1
 
 snapnum=`printf '%04d' ${SLURM_ARRAY_TASK_ID}`
-basedir=/cosma5/data/jch/HaloProperties/200_w_lightcone/
+basedir=/cosma7/data/dp004/jch/FLAMINGO/L0200N0360/HYDRO_FIDUCIAL/
 
-swift_filename="${basedir}/snapshots/flamingo_${snapnum}.hdf5"
-vr_basename="${basedir}/vr/catalogue_${snapnum}/vr_catalogue_${snapnum}"
-outfile="${basedir}/group_membership/vr_membership_${snapnum}.hdf5"
+swift_filename="${basedir}/flamingo_${snapnum}/flamingo_${snapnum}.%(file_nr)d.hdf5"
+vr_basename="${basedir}/VR/catalogue_${snapnum}/vr_catalogue_${snapnum}"
+outfile="${basedir}/SOAP/group_membership/vr_membership_${snapnum}.%(file_nr)d.hdf5"
 
 mpirun python3 -u -m mpi4py \
     ./vr_group_membership.py ${swift_filename} ${vr_basename} ${outfile}
--- scripts/test_halo_properties.sh
+++ scripts/test_halo_properties.sh
@@ -1,10 +1,11 @@
 #!/bin/bash -l
 #
 #SBATCH --nodes=1
+#SBATCH --tasks-per-node=16
 #SBATCH --cpus-per-task=1
 #SBATCH -J test_halo_properties
 #SBATCH -o ./logs/test_halo_properties.%a.out
-#SBATCH -p cosma
+#SBATCH -p cordelia
 #SBATCH -A durham
 ##SBATCH --exclusive
 #SBATCH -t 2:00:00
@@ -13,15 +14,17 @@
 module purge
 module load gnu_comp/11.1.0 openmpi/4.1.1 python/3.10.1
 
-basedir="/cosma5/data/jch/HaloProperties/200_w_lightcone/"
+snapnum=`printf '%04d' ${SLURM_ARRAY_TASK_ID}`
+basedir=/cosma7/data/dp004/jch/FLAMINGO/L0200N0360/HYDRO_FIDUCIAL/
 
-swift_filename="${basedir}/snapshots/flamingo_%(snap_nr)04d.hdf5"
-vr_basename="${basedir}/vr/catalogue_%(snap_nr)04d/vr_catalogue_%(snap_nr)04d"
-nr_chunks=1
-outfile="${basedir}/halo_properties/halo_properties_%(snap_nr)04d.hdf5"
-extra_filename="${basedir}/group_membership/vr_membership_%(snap_nr)04d.hdf5"
+swift_filename="${basedir}/flamingo_${snapnum}/flamingo_${snapnum}.%(file_nr)d.hdf5"
+vr_basename="${basedir}/VR/catalogue_${snapnum}/vr_catalogue_${snapnum}"
+nr_chunks=8
+scratch_dir="${basedir}/SOAP/chunks-tmp/"
+outfile="${basedir}/SOAP/halo_properties/halo_properties_%(snap_nr)04d.hdf5"
+extra_filename="${basedir}/SOAP/group_membership/vr_membership_%(snap_nr)04d.%(file_nr)d.hdf5"
 
 mpirun python3 -u -m mpi4py \
-    ./compute_halo_properties.py ${swift_filename} ${vr_basename} ${outfile} ${SLURM_ARRAY_TASK_ID} \
+    ./compute_halo_properties.py ${swift_filename} ${scratch_dir} ${vr_basename} ${outfile} ${SLURM_ARRAY_TASK_ID} \
     --chunks=${nr_chunks} \
     --extra-input=${extra_filename}
