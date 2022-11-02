6d3c51d5892b8b564b6210bb5907422c5b053e6b
Bert Vandenbroucke <vandenbroucke@strw.leidenuniv.nl>
2022-11-02 10:25:11 +0100

    Added version string and correct timestamp to SOAP documentation.

--- property_table.py
+++ property_table.py
@@ -2,6 +2,22 @@
 
 import numpy as np
 import unyt
+import subprocess
+import datetime
+import os
+
+
+def get_version_string():
+    handle = subprocess.run("git describe --always", shell=True, stdout=subprocess.PIPE)
+    if handle.returncode != 0:
+        git_version = "Unknown SOAP version"
+    else:
+        git_version = handle.stdout.decode("utf-8").strip()
+        git_version = f"SOAP version ``{git_version}''"
+    timestamp = datetime.datetime.now().strftime("%A %-d %B %Y, %H:%M:%S")
+    username = os.getlogin()
+    hostname = os.uname().nodename
+    return f"{git_version} -- Compiled by user ``{username}'' on {hostname} on {timestamp}."
 
 
 class PropertyTable:
@@ -1189,7 +1205,7 @@ class PropertyTable:
             )
         print("}")
 
-    def print_table(self, tablefile, footnotefile):
+    def print_table(self, tablefile, footnotefile, timestampfile):
         prop_names = sorted(
             self.properties.keys(),
             key=lambda key: (
@@ -1269,6 +1285,8 @@ Name & Shape & Type & Units & SH & ES & IS & EP & SO & Category & Compression\\\
         tablestr += """\\end{longtable}
 \\end{landscape}"""
         tailstr = "\\end{document}"
+        with open(timestampfile, "w") as ofile:
+            ofile.write(get_version_string())
         with open(tablefile, "w") as ofile:
             ofile.write(tablestr)
         with open(footnotefile, "w") as ofile:
@@ -1305,4 +1323,8 @@ if __name__ == "__main__":
     if False:
         table.print_dictionary()
     else:
-        table.print_table("documentation/table.tex", "documentation/footnotes.tex")
+        table.print_table(
+            "documentation/table.tex",
+            "documentation/footnotes.tex",
+            "documentation/timestamp.tex",
+        )
