b8584db8ca9a9fd21aac0906a18a267b9a2441fc
Bert Vandenbroucke <vandenbroucke@strw.leidenuniv.nl>
2022-11-10 11:10:01 +0100

    Made sure projected velocity dispersions are mass-weighted.

--- projected_aperture_properties.py
+++ projected_aperture_properties.py
@@ -322,7 +322,7 @@ class SingleProjectionProjectedApertureParticleData:
             return None
         proj_vgas = self.proj_velocity[self.proj_type == "PartType0", self.iproj]
         vcom_gas = (self.gas_mass_fraction * proj_vgas).sum()
-        return np.sqrt(((proj_vgas - vcom_gas) ** 2).sum())
+        return np.sqrt((self.gas_mass_fraction * (proj_vgas - vcom_gas) ** 2).sum())
 
     @lazy_property
     def ProjectedGasAxisLengths(self):
@@ -344,7 +344,7 @@ class SingleProjectionProjectedApertureParticleData:
             return None
         proj_vdm = self.proj_velocity[self.proj_type == "PartType1", self.iproj]
         vcom_dm = (self.dm_mass_fraction * proj_vdm).sum()
-        return np.sqrt(((proj_vdm - vcom_dm) ** 2).sum())
+        return np.sqrt((self.dm_mass_fraction * (proj_vdm - vcom_dm) ** 2).sum())
 
     @lazy_property
     def star_mass_fraction(self):
@@ -358,7 +358,7 @@ class SingleProjectionProjectedApertureParticleData:
             return None
         proj_vstar = self.proj_velocity[self.proj_type == "PartType4", self.iproj]
         vcom_star = (self.star_mass_fraction * proj_vstar).sum()
-        return np.sqrt(((proj_vstar - vcom_star) ** 2).sum())
+        return np.sqrt((self.star_mass_fraction * (proj_vstar - vcom_star) ** 2).sum())
 
     @lazy_property
     def ProjectedStellarAxisLengths(self):
