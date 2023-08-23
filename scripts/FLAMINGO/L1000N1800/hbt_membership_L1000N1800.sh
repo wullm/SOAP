b52287cc7ff7b1405cecaeee45dbc4d81d642368
John Helly <j.c.helly@durham.ac.uk>
2023-08-23 16:41:37 +0100

    Add scripts to run on HBTplus and Subfind

---
+++ scripts/FLAMINGO/L1000N1800/hbt_membership_L1000N1800.sh
@@ -0,0 +1,48 @@
+#!/bin/bash -l
+#
+# Compute VR group membership for each particle in a snapshot.
+# Job name determines which of the L1000N1800 runs we process.
+# Array job index is the snapshot number to do.
+#
+# Submit with (for example):
+#
+# sbatch -J HYDRO_FIDUCIAL --array=78 ./group_membership_L1000N1800.sh
+#
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --tasks-per-node=64
+#SBATCH -o ./logs/hbt_membership_L1000N1800_%x.%a.out
+#SBATCH -p cosma8-shm2
+#SBATCH -A dp004
+#SBATCH --exclusive
+#SBATCH -t 0:30:00
+#
+
+module purge
+module load gnu_comp/11.1.0 openmpi/4.1.1 python/3.10.1
+
+# Which snapshot to do
+snapnum=`printf '%04d' ${SLURM_ARRAY_TASK_ID}`
+snapnum3=`printf '%03d' ${SLURM_ARRAY_TASK_ID}`
+
+# Which simulation to do
+sim="L1000N1800/${SLURM_JOB_NAME}"
+
+# Input simulation location
+basedir="/cosma8/data/dp004/flamingo/Runs/${sim}/"
+
+# Where to write the output
+outbase="/cosma8/data/dp004/jch/FLAMINGO/ScienceRuns/${sim}/HBT/SOAP/"
+
+# Generate input and output file names
+swift_filename="${basedir}/snapshots/flamingo_${snapnum}/flamingo_${snapnum}.%(file_nr)d.hdf5"
+hbt_basename="/snap8/scratch/dp004/jch/FLAMINGO/HBT/L1000N1800_HYDRO_FIDUCIAL/hbt/${snapnum3}/SubSnap_${snapnum3}"
+outfile="${outbase}/group_membership_${snapnum}/vr_membership_${snapnum}.%(file_nr)d.hdf5"
+
+# Create output directory
+outdir=`dirname "${outfile}"`
+mkdir -p "${outdir}"
+lfs setstripe --stripe-count=-1 --stripe-size=32M ${outdir}
+
+mpirun python3 -u -m mpi4py \
+       ./group_membership.py ${swift_filename} ${hbt_basename} HBTplus ${outfile}
